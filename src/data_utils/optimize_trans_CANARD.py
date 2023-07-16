dc001dd7f266a70b3cd8d3b7fe2c6dddd29efb63
whoisltd <tiendat.le129@gmail.com>
2023-07-17 01:03:20 +0700

    update optimize_trans_CANARD.py

--- src/data_utils/optimize_trans_CANARD.py
+++ src/data_utils/optimize_trans_CANARD.py
@@ -7,7 +7,7 @@ from joblib import Parallel, delayed
 
 def config():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file_text', default="test.json")
+    parser.add_argument('--file_text', default="dev.json")
     parser.add_argument('--file_name_save', default='dev_vi.json')
     args = parser.parse_args()
     return args
@@ -16,7 +16,7 @@ def main():
     args = config()
     json_file = args.file_text
 
-    with open(json_file, 'r', encoding='utf8') as f:
+    with open(json_file, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
     dialog_ids = [i['QuAC_dialog_id'] for i in data]
@@ -24,14 +24,14 @@ def main():
 
     total_batch = divide_batch(data)
 
-    results = Parallel(n_jobs=-1, prefer="threads")(delayed(translate_batch)(i) for i in tqdm(total_batch[:100]))
+    results = Parallel(n_jobs=-1, prefer="threads")(delayed(translate_batch)(i) for i in tqdm(total_batch))
 
     # final_result = []
     # for x in results: final_result.extend(x)
-    final_result = combine_batch(results)
+    final_result = combine_batches(results)
 
-    with open(args.file_name_save, 'w', encoding='utf8') as f:
-        json.dump(final_result, f)
+    with open(args.file_name_save, 'w', encoding='utf-8') as f:
+        json.dump(final_result, f, ensure_ascii=False)
 
 def divide_batch(data):
     temp_dialog_id = data[0]['QuAC_dialog_id']
@@ -49,26 +49,17 @@ def divide_batch(data):
 
 def translate_batch(data):
     translator = Translator(service_urls = ["translate.googleapis.com"])
-    temp = []
     total_result = []
+    temp_his_trans = {data[-1]['History'][index]: value.text for index, value in enumerate(translator.translate(data[-1]['History'], dest='vi'))}
+
     for idx in range(len(data)):
         dict_ = {}
-        if idx == 0:
-            for history in data[0]['History']:
-                temp.append(translator.translate(history, dest='vi').text)
-            dict_['History'] = temp
-            dict_['QuAC_dialog_id'] = data[0]['QuAC_dialog_id']
-            dict_['Question_no'] = data[0]['Question_no']
-            dict_['Question'] = translator.translate(data[0]['Question'], dest='vi').text
-            dict_['Rewrite'] = translator.translate(data[0]['Rewrite'], dest='vi').text
-            total_result.append(dict_)
-        else:
-            dict_['History'] = total_result[-1]['History'] + [translator.translate(data[idx]['History'][-1], dest='vi').text]
-            dict_['QuAC_dialog_id'] = data[idx]['QuAC_dialog_id']
-            dict_['Question_no'] = data[idx]['Question_no']
-            dict_['Question'] = translator.translate(data[idx]['Question'], dest='vi').text
-            dict_['Rewrite'] = translator.translate(data[idx]['Rewrite'], dest='vi').text
-            total_result.append(dict_)
+        dict_['History'] = [temp_his_trans[key] for key in data[idx]['History']]
+        dict_['QuAC_dialog_id'] = data[idx]['QuAC_dialog_id']
+        dict_['Question_no'] = data[idx]['Question_no']
+        dict_['Question'] = temp_his_trans[data[idx]['Question']] if data[idx]['Question'] in temp_his_trans else translator.translate(data[idx]['Question'], dest='vi').text
+        dict_['Rewrite'] = translator.translate(data[idx]['Rewrite'], dest='vi').text
+        total_result.append(dict_)
     return total_result
 
 def combine_batches(results):
