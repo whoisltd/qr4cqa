4c32a0e51cf7a00d6159b345b09c657b78fbd1de
hopbv <vanhop3499@gmail.com>
2023-07-17 00:54:01 +0700

    update readme

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash                                                                     
-git submodule sync
-git submodule update --init --recursive
-echo "Done sync"
-
-git pull && git submodule update
-echo "Pull and update submodule: Done "
